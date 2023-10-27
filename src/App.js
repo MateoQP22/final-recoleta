aa4edd2541c1c3831bc6fef829ae05d0b29bb4e7
sistemasisosgruop <sistemasisosgroup@gmail.com>
2023-10-26 21:40:52 -0500

    Deployando React

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h2>Curso de Titulacion</h2>
+          <p>Mateo Quispe Pacheco</p>
         </p>
         <a
           className="App-link"
