b7bc4d4e1c6f7b0ea6ca6d906e835c3d0cca0477
MateoQP22 <73276257@continental.edu.pe>
2023-11-07 15:15:54 -0500

    Componentes de variables y matrices

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import CO1componente from './components/CO1componente';
 import CO2contador from './components/CO2contador';
+import Variables from './components/CO4variables';
+import CO6matriz from './components/CO6matriz';
 
 function App() {
   return (
     <div>
       <CO1componente/>
       <CO2contador/>
+      <Variables xVariable="Hola mundo"/>
+      <CO6matriz/>
     </div>
   );
 }
