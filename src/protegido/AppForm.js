db45c0dc9187eafd981c3d8bf22a66eefae618ea
MateoQP22 <73276257@continental.edu.pe>
2023-11-14 16:50:14 -0500

    Formulario con Agregar, Leer, Eliminar

--- src/protegido/AppForm.js
+++ src/protegido/AppForm.js
@@ -1,20 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../conexion/firebase"
+
 
 const AppForm = (props) => {
+  const handleStatusChange = (e) =>{
+    const {name,value} = e.target
+    setObjeto({...objeto,[name]:value});
+    console.log({name , value});
+  }
+
+  const camposRegistro = { nombre:"", edad:"", genero:""};
+  const [objeto, setObjeto] = useState(camposRegistro);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      if(props.idActual == ""){
+        if(validarForm()){
+          addDoc(collection(db, 'persona'),objeto);
+          alert("Se registro con exito...");
+        }else{
+          console.log("No se Guardo...")
+        }
+      }else{
+        alert("No se pudo guardar")
+      }
+    } catch (error) {
+      console.log("Error en CREAR o ACTUALIZAR", error);
+    }
+  }
+
+  const validarForm = () => {
+    if (objeto.nombre === "" || /^\s+$/.test(objeto.nombre)){
+      alert("Esribe tu nombre...")
+      return false;
+    }
+    return true;
+  };
+
   return (
     <div style={{background:"orange", padding:"10px"}}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <button>Cerrar Aplicación</button>
         <h2>Registrar (AppForm.js)</h2>
-        <input name='nombre' type='text' placeholder='Nombre'/><br/>
-        <input name='edad' type='text' placeholder='Edad'/><br/>
-        <select>
+        <input onChange={handleStatusChange} name='nombre' type='text' placeholder='Nombre'/><br/>
+        <input onChange={handleStatusChange} name='edad' type='text' placeholder='Edad'/><br/>
+        <select onChange={handleStatusChange} name='genero'>
           <option value="">Seleccione una opción</option>
           <option value="Masculino">Masculino</option>
           <option value="Femenino">Femenino</option>
         </select>
         <br/>
-        <button>Guardar o Actualizar</button>
+        <button >
+          {props.idActual=="" ? "Guardar" : "Actualizar"}
+        </button>
       </form>
     </div>
   )
--- src/protegido/AppLista.js
+++ src/protegido/AppLista.js
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AppForm from './AppForm'
+import { collection, deleteDoc, doc, onSnapshot, query } from 'firebase/firestore';
+import { db } from '../conexion/firebase';
 
 const AppLista = (props) => {
+  const [docBD, setDocBD] = useState([]);
+  const frRead = () => {
+    const xColeccionConQuery = query(collection(db, 'persona'));
+    const unsubcribe = onSnapshot(xColeccionConQuery, (xDatosBD) =>{
+      const xDoc = [];
+      xDatosBD.forEach((doc)=>{
+        xDoc.push({id:doc.id, ...doc.data()});
+      });
+      setDocBD(xDoc);
+    });
+  }
+  frRead();
+
+  const [idActual, setIdActual] = useState("");
+  const fnDelete = async (xId) => {
+      if(window.confirm("Confirme para eliminar")){
+          await deleteDoc(doc(db, "persona", xId));
+      }
+    alert("Se elimino con éxito...");
+  }
   return (
     <div style={{background:"greenyellow", padding:"10px"}}>
       <h1>AppLista</h1>
-      <AppForm/>
+      <AppForm {...{idActual, setIdActual}}/>
       <h3>Lista de Clientes</h3>
-      <p>No. 1. Juan Manuel Ticona Vega ..... x ..... A</p>
-      <p>No. 2. Rosa Maria Luque Conde ..... x ..... A</p>
-      <p>No. 3. Joe Ricardo Lopez Moral ..... x ..... A</p>
+      {
+        docBD.map((data, index) => 
+        <p key={data.id}>
+          No. {index + 1}. {data.nombre} 
+          ..............
+          <span onClick={() => fnDelete(data.id)}>❌</span>
+          ..............
+          <span onClick={() => setIdActual(data.id)}>📝</span>
+        </p>
+        )
+      }
     </div>
   )
 }
