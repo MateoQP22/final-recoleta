909a4debff523242201f8375ceb1bb36104afe3d
MateoQP22 <73276257@continental.edu.pe>
2023-11-15 12:14:48 -0500

    Termino de proyecto funcionalidad

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
-import AppLista from './protegido/AppLista';
-import './css/estilos.css';
+import AppLista from './protegido/sistemacrud/AppLista';
+import Dashboard from './public/Dashboard';
+import Home from './public/Home';
+import BarraRutasPublic from './ruteo/BarraRutasPublic';
+import BarraRutasProtected from './ruteo/BarraRutasProtected';
+import { useAuth } from "./ruteo/AuthContext"
 
 function App() {
+  const { user } = useAuth();
+
   return (
-    <div style={{background:"violet", width:"350px",padding:"10px"}}>
-        <h1>App</h1>
-        <AppLista/>
+    <div style={{ background: "violet", padding: "10px" }}>
+      <h1>App</h1>
+      <Router>
+        {user ? <BarraRutasProtected /> : <BarraRutasPublic />}
+      </Router>
     </div>
   );
 }
--- src/conexion/firebase.js
+++ src/conexion/firebase.js
@@ -1,32 +1,31 @@
-// USUARIO : mateo@gmail.com
-// Contraseña : Mateo12345
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBU8vioH5Nv4BB9C8ii0uSmYOE_SuUFLNg",
-  authDomain: "recoleta-acceso.firebaseapp.com",
-  databaseURL: "https://recoleta-acceso-default-rtdb.firebaseio.com",
-  projectId: "recoleta-acceso",
-  storageBucket: "recoleta-acceso.appspot.com",
-  messagingSenderId: "428991546423",
-  appId: "1:428991546423:web:e10e6c7876bec713262bb2"
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN, 
+  databaseURL: process.env.REACT_APP_DATABASEURL,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const auth    = getAuth(app);
+export const db      = getFirestore(app);
 export const storage = getStorage(app);
-
